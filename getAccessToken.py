5e1e19d00d0e239cf45a36c8779630adf120148d
ihappier <ihappier@live.com>
2018-07-12 15:15:12 +0800

    update

--- getAccessToken.py
+++ getAccessToken.py
@@ -1,40 +1,40 @@
-import requests as nq 
-
+import requests as nq
 
 netAdress = '180.101.147.89'
 port = '8743'
 
 appID = '4CFpzk3vFme73dJCogtqac6H1EIa'
 secret = '6c1qCXEzcp0F0l47iWtvuLYsqlwa'
-cert = ('D:\智慧热网\\2018\getdata\dianxin\dianxin\cert\client.crt','D:\智慧热网\\2018\getdata\dianxin\dianxin\cert\client.key')
+cert = (
+    'D:\智慧热网\\2018\getdata\dianxin\dianxin\cert\client.crt', 'D:\智慧热网\\2018\getdata\dianxin\dianxin\cert\client.key')
 
 
-def conectIoT(appID, secret, cert, netAdress = '180.101.147.89', port = '8743'):
+def conectLot(appID, secret, cert, netAdress='180.101.147.89', port='8743'):
     """接入平台"""
     url = 'https://' + netAdress + ':' + port + '/iocm/app/sec/v1.1.0/login'
-    r = nq.post(url ,data={'appId':appID,'secret':secret},cert = cert, verify=False)
+    r = nq.post(url, data={'appId': appID, 'secret': secret}, cert=cert, verify=False)
     if r.status_code == nq.codes.ok:
         d = eval(r.text)
         if d['accessToken']:
             return d
         else:
-            return 
+            return
 
 
-def refreshToken(appID, secret,cert,refreshToken,netAdress = '180.101.147.89',port = '8743'):
+def refreshToken(appID, secret, cert, refreshToken, netAdress='180.101.147.89', port='8743'):
     """刷新鉴权Token"""
     url = 'https://' + netAdress + ':' + port + '/iocm/app/sec/v1.1.0/refreshToken'
-    r = nq.post(url, data={'appId':appID, 'secret':secret, 'refreshToken':refreshToken}, cert=cert, verify=False)
+    r = nq.post(url, data={'appId': appID, 'secret': secret, 'refreshToken': refreshToken}, cert=cert, verify=False)
     if r.status_code == nq.codes.ok:
-        d =eval(r.text)
+        d = eval(r.text)
         return d
 
 
-def getDevicesMsg(appID, accessToken, deviceID, cert, netAdress = '180.101.147.89', port='8743'):
+def getDevicesMsg(appID, accessToken, deviceID, cert, netAdress='180.101.147.89', port='8743'):
     """获取已连接设备信息,返回list形式，list[0]是设备ID，list[1]是设备是否激活(true,false)，list[2]是设备名称"""
-    url = 'https://' + netAdress + ':' + port + '/iocm/app/reg/v1.1.0/deviceCredentials/'+ deviceID
-    itemHeaders =  {"app_key":appID, "Authorization": "Bearer "+accessToken,"Content-Type":"applicaton/json"}
-    m = nq.get( url,headers = itemHeaders, cert = cert, verify=False)
+    url = 'https://' + netAdress + ':' + port + '/iocm/app/reg/v1.1.0/deviceCredentials/' + deviceID
+    itemHeaders = {"app_key": appID, "Authorization": "Bearer " + accessToken, "Content-Type": "applicaton/json"}
+    m = nq.get(url, headers=itemHeaders, cert=cert, verify=False)
     if m.status_code == nq.codes.ok:
         d = eval(m.text)
         if d['deciveId']:
@@ -44,13 +44,29 @@ def getDevicesMsg(appID, accessToken, deviceID, cert, netAdress = '180.101.147.8
     else:
         return '页面不存在'
 
-def registerDevice(appID, nodeId,accessToken,cert,netAdress = '180.101.147.89', port='8743'):
+
+def registerDevice(appID, nodeId, accessToken, cert, netAdress='180.101.147.89', port='8743'):
     """注册设备"""
-    url = 'https://' + netAdress + ':' + port + '/iocm/app/reg/v1.1.0/deviceCredentials?appId='+appID
-    itemHeaders = {"app_key":appID, "Authorization": "Bearer "+accessToken,"Content-Type":"applicaton/json"}
-    r = nq.post(url=url,DeprecationWarning={'nodeId':nodeId},headers=itemHeaders,cert=cert,verify=False)
+    url = 'https://' + netAdress + ':' + port + '/iocm/app/reg/v1.1.0/deviceCredentials?appId=' + appID
+    itemHeaders = {"app_key": appID, "Authorization": "Bearer " + accessToken, "Content-Type": "applicaton/json"}
+    r = nq.post(url=url, DeprecationWarning={'nodeId': nodeId}, headers=itemHeaders, cert=cert, verify=False)
     if r.status_code == nq.codes.ok:
         return eval(r.text)
 
-def refreshVerifyCode(appID, nodeId,accessToken,cert,netAdress = '180.101.147.89', port='8743'):
+
+def refreshVerifyCode(appID, nodeId, accessToken, cert, netAdress='180.101.147.89', port='8743'):
     """刷新"""
+    url = 'https://' + netAdress + ':' + port + '/iocm/app/reg/v1.1.0/deviceCredentials?appId=' + appID
+    return d
+
+
+def modfyDevice(appID, nodeId, accessToken, cert, netAdress='180.101.147.89', port='8743'):
+    """"修改设备"""
+    return
+
+
+def deleteDevice(appID, nodeId, accessToken, cert, netAdress='180.101.147.89', port='8743'):
+    return
+
+
+def
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ port = '8743'
 url = 'https://' + netAdress + ':' + port + '/iocm/app/sec/v1.1.0/login'
 appID = '4CFpzk3vFme73dJCogtqac6H1EIa'
 sectet = '6c1qCXEzcp0F0l47iWtvuLYsqlwa'
-cert = ('D:\智慧热网\\2018\getdata\dianxin\dianxin\cert\client.crt','D:\智慧热网\\2018\getdata\dianxin\dianxin\cert\client.key')
+cert = ('D:\智慧热网\\2018\iot\cert\client.crt', 'D:\智慧热网\\2018\iot\cert\client.key')
 r = nq.post(url ,data={'appId':'4CFpzk3vFme73dJCogtqac6H1EIa','secret':'6c1qCXEzcp0F0l47iWtvuLYsqlwa'},cert = cert, verify=False)
 d = eval(r.text)
 print (r.status_code)
